bf693fa39d98b5e266cf26d2c0c1a8608fdbc2d5
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2018-03-04 22:48:40 +0200

    Test

--- entry.cpp
+++ entry.cpp
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#include <cstdio>
 
 int main(int argc, char *argv[]) {
-    printf("foo%c", '\n');
+	// CUDA 9.1 does not support cl of version 19.12
+	// Alternative: use toolset v140, cl version: 19.0
+	printf("cl ver: %d", _MSC_VER);
+	getchar();
 }
