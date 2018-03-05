fb2bf7cd7ed93f7f71b83021ffc41f6794ab14f2
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2018-03-05 20:47:27 +0200

    rm getchar

--- entry.cpp
+++ entry.cpp
@@ -4,5 +4,4 @@ int main(int argc, char *argv[]) {
 	// CUDA 9.1 does not support cl of version 19.12
 	// Alternative: use toolset v140, cl version: 19.0
 	printf("cl ver: %d", _MSC_VER);
-	getchar();
 }
