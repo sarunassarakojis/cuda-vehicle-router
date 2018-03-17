e3b2862dcdd4c610026c17577ec6d302b272fd47
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2018-03-17 21:17:25 +0200

    Fail on lower CMake version than 3.10

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 	// CUDA 9.1 does not support cl of version 19.12
 	// Alternative: use toolset v140, cl version: 19.0
-	printf("cl ver: %d", _MSC_VER);
+	printf("cl ver: %d%c", _MSC_VER, '\n');
 }
