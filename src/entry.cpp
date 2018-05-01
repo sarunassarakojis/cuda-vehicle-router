6041db9e97ebbeb9c389fb32295fd2133b94be89
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2018-05-01 18:41:18 +0300

    Add logging library

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,5 +1,12 @@
 #include "routing/router_library.h"
+#include "utilities/device/device_query.h"
 
 int main(int argc, char* argv[]) {
+    using namespace device_types;
+
+    Device_properties* properties = nullptr;
+    unsigned size = 0;
+
     vr::execute_in_parallel();
+    device_query::get_cuda_device_properties(properties, size);
 }
--- src/utilities/device/device_query.cpp
+++ src/utilities/device/device_query.cpp
@@ -1,5 +1,10 @@
+#include <spdlog/spdlog.h>
 #include "utilities/device/device_query.h"
 
 using namespace device_types;
 
-void device_query::get_cuda_device_properties(Device_properties* devices, unsigned& size) {}
+void device_query::get_cuda_device_properties(Device_properties* devices, unsigned& size) {
+    auto logger = spdlog::stdout_color_mt("console");
+
+    logger->info("Attempting to get cuda devices");
+}
